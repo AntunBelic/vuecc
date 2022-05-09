53c8dc45c9cb54575540a2fff0aa067d4316a5c0
AntunBelic <antunbelic93@gmail.com>
2022-05-09 17:54:41 +0200

    todosComponent

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,45 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <TodosComponent />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TodosComponent from "./components/TodosComponent.vue"
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    TodosComponent
+},
+  data(){
+    return {
+      todos:[
+        {
+          id:1,
+          title:"Todo One",
+          completed:false
+        },
+        {
+          id:2,
+          title:"Todo Two",
+          completed:false
+        },
+        {
+          id:3,
+          title:"Todo Three",
+          completed:false
+        }
+      ]
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
 }
 </style>
